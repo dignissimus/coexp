857d3de82055c80a44a9d115e2d47809918fda7b
Sam Ezeh <sam.z.ezeh@gmail.com>
2024-01-06 03:41:37 +0000

    Add integer value

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use crate::Statement::*;
 use colored::Colorize;
 use std::collections::HashMap;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Expression {
     Function {
         parameter: String,
@@ -18,20 +18,27 @@ enum Expression {
     Name(String),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Statement {
     Assignment { name: String, value: Expression },
     Data { name: String, cases: Vec<DataCase> },
     Debug { expression: Expression },
+    DebugAst { expression: Expression },
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct DataCase {
     name: String,
 }
 
+#[derive(Debug, Clone)]
+enum Value {
+    Integer(i64),
+}
+
 type Program = Vec<Statement>;
 
+#[derive(Clone)]
 struct ProgramContext {
     variables: HashMap<String, Expression>,
 }
@@ -43,13 +50,13 @@ impl ProgramContext {
         }
     }
 
-    fn run_program(&mut self, program: Program) {
+    fn run_program(self, program: Program) -> ProgramContext {
         program
             .iter()
-            .for_each(|statement| self.evaluate_statement(&statement));
+            .fold(self, |context, statement| self.evaluate_statement(&statement))
     }
 
-    fn evaluate_statement(&mut self, statement: &Statement) {
+    fn evaluate_statement(&self, statement: &Statement) -> ProgramContext {
         match statement {
             Assignment { name, value } => {
                 println!(
@@ -59,38 +66,68 @@ impl ProgramContext {
                     name.blue(),
                     "=",
                     format!("{:?}", value).red()
-                )
+                );
+                ProgramContext {..self.clone()}
+            }
+            DebugAst { expression } => {
+                println!(
+                    "{} {}",
+                    "[debug]:".cyan(),
+                    format!("{:?}", expression).blue()
+                );
+                ProgramContext {..self.clone()}
             }
             Debug { expression } => {
                 println!(
                     "{} {}",
                     "[debug]:".cyan(),
-                    format!("{:?}", expression).blue()
-                )
+                    match expression {
+                        Name(name) => {
+                            let expression = self.variables[name];
+                            let value = self.evaluate(&expression);
+                            format!("{:?}", value).blue()
+                        }
+                        _ => todo!(),
+                    }
+                );
+                ProgramContext {..self.clone()}
             }
             _ => todo!(),
         }
     }
 
-    fn evaluate(&mut self, _expression: &Expression) {}
+    fn evaluate(&self, expression: &Expression) -> Value {
+        match expression {
+            IntegerLiteral(int) => Value::Integer(*int),
+            _ => todo!()
+        }
+    }
 }
 
 fn main() {
     let program: Program = vec![
         Assignment {
-            name: "f".to_string(),
+            name: "identity".to_string(),
             value: Function {
                 parameter: "x".to_string(),
                 body: Box::new(Name("x".to_string())),
             },
         },
-        Debug {
+        DebugAst {
             expression: IntegerLiteral(5),
+        },
+        Assignment {
+            name: "value".to_string(),
+            value: Call {
+                name: "identity".to_string(),
+                argument: Box::new(IntegerLiteral(5)),
+            },
+        },
+        Debug {
+            expression: Name("value".to_string()),
         },
     ];
 
     let mut context = ProgramContext::new();
     context.run_program(program);
-
-    println!("Hello, world!");
 }
